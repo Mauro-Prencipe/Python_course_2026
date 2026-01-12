ef3a9b258882460e1a073252de40012f961588a0
Mauro Prencipe <mauro.prencipe@gmail.com>
2026-01-12 16:08:16 +0100

    Update exercise_1.py

--- lectures/exercises/exercise_1.py
+++ lectures/exercises/exercise_1.py
@@ -1,5 +1,4 @@
 # Exercise 1
-# 10/02/2025
 
 # Read a list from the file "ex_1_data_in.dat" file. 
 # Take the square root of each element of the list and 
@@ -7,5 +6,6 @@
 # Such file must have two columns: the first one should
 # contain the original list; the second one the computed
 # square root.
+
 
 
